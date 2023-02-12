49d674b879f99571b1f7363a33a2ce4d8f363f65
Dhruv Chaturvedi <dc2020@hw.ac.uk>
2023-02-12 16:53:11 +0400

    added local host db comment in connect file

--- connect.js
+++ connect.js
@@ -2,10 +2,21 @@ const mysql = require("mysql")
 
 const db = mysql.createPool({
 
-    host:"sg3plcpnl0125.prod.sin3.secureserver.net",
-    user: "akshay_year3",
-    password: "MemAkshay123",
-    database: "memories_ar",
+   host:"sg3plcpnl0125.prod.sin3.secureserver.net",
+   user: "akshay_year3",
+   password: "MemAkshay123",
+   database: "memories_ar",
 })
 
 module.exports = db
+
+// const mysql = require("mysql")
+
+// const db = mysql.createPool({
+
+//     host:"localhost",
+//     user: "root",
+//     database: "memories_ar",
+// })
+
+// module.exports = db
