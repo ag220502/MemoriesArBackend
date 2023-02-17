9070d2c4cc96b31775e4bd98b74e13f9f8bc734a
Davey <91110994+dhd2000@users.noreply.github.com>
2023-02-17 22:04:55 +0400

    dislikePosts implemented
    works with postman

--- routes/Posts/dislikePost.js
+++ routes/Posts/dislikePost.js
@@ -1,1 +1,12 @@
 //Routes for Dislike Post and and undislike Post
+const express = require("express");
+const router = express.Router();
+const {
+  dislikePost,
+  undislikePost,
+} = require("../../controllers/Posts/dislikePost")
+
+router.post("/dislike", dislikePost);
+router.delete("/undislike", undislikePost);
+
+module.exports = router;
