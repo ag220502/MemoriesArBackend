351f0fcd35303060d2877ab5d7edefa62c852f3a
Davey <91110994+dhd2000@users.noreply.github.com>
2023-02-17 21:22:33 +0400

    commentPost implemented
    have to recheck, works with postman though

--- routes/Posts/commentPost.js
+++ routes/Posts/commentPost.js
@@ -1,1 +1,12 @@
 // Routes for Comment on Post, and Delete Comment
+const express = require("express");
+const router = express.Router();
+const {
+  postComment,
+  deleteComment,
+} = require("../../controllers/Posts/commentPost.js")
+
+router.post("/post", postComment);
+router.delete("/delete", deleteComment);
+
+module.exports = router;
