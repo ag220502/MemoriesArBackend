6fc3b923bc311302ddbf6d8bec672ea2fb5461d6
Dhruv Chaturvedi <dc2020@hw.ac.uk>
2023-03-04 10:01:10 +0400

    implemented recent searches CRUD

---
+++ routes/Users/recentSearches.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+const func = require("../../controllers/Users/recentSearches.js")
+
+router.post("/createSearch",func.createSearch)
+router.patch("/deleteSearch",func.deleteSearch)
+router.get("/userRecentSearches",func.userRecentSearches)
+
+module.exports = router
