7af22a76d109fcb233304397c1750f76d924e6a4
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-06 12:39:57 +0400

    forgot pasword

--- routes/Users/auth.js
+++ routes/Users/auth.js
@@ -6,7 +6,8 @@ router.post("/login",func.loginFunc)
 router.post("/register",func.registerFunc)
 router.get("/logout",func.logoutFunc)
 router.post("/forgotPass",func.forgotFunc)
+router.patch("/resetPass",func.resetPassFunc)
 router.patch("/updatePass",func.updatePassword)
 router.post("/getId",func.getId)
 
-module.exports = router
+module.exports = router 
