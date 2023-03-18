211e80d3afa6b2f842c6d16661a75c134afcea00
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-18 15:39:54 +0400

    firebaseFunc

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,10 @@ const queries = require("../crudOperations/Posts/userPost");
 const profileQueries = require("../crudOperations/Users/Profile/ProfilePage");
 const tagQueries = require("../crudOperations/Posts/tagPost");
 
-const functions = require("firebase-functions");
 const { Storage } = require("@google-cloud/storage");
 const UUID = require("uuid-v4");
 const express = require("express");
 const formidable = require("formidable-serverless");
-require("dotenv").config();
 
 const app = express();
 app.use(express.json({ limit: "50mb", extended: true }));
@@ -16,9 +14,7 @@ app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 var admin = require("firebase-admin");
 
-
 var serviceAccount = require("../admin.json");
-const router = express.Router();
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -32,90 +28,60 @@ const storage = new Storage({
 // "https://firebasestorage.googleapis.com/v0/b/memoriesar-f08a7.appspot.com/o/";
 // const bucket = storage.bucket("gs://memoriesar-f08a7.appspot.com");
 
+// we can move this to the router now
 const createPost = async (req, res) => {
   const form = new formidable.IncomingForm({ multiples: true });
 
   try {
     form.parse(req, async (error, fields, files) => {
-      // create a unique id for the image
-      let uuid = UUID();
-
-      // url of the uploaded image
-      var downLoadPath =
-        "https://firebasestorage.googleapis.com/v0/b/memoriesar-f08a7.appspot.com/o/";
-      
-      
-        const postImage = files.postImage;
-      if (!postImage) {
-        return res.status(404).json("No image found");
-      }
-      // rename the image
-      postImage.name = uuid + postImage.name;
-
-      // url of the uploaded image
-      let imageUrl;
-
+     
       // check for errors
       if (error) {
         return res.status(400).json(error);
       }
-
-      // get the bucket
-      const bucket = storage.bucket("gs://memoriesar-f08a7.appspot.com");
-
-      // check if image is uploaded
-      if (postImage.size == 0) {
-        return res.status(404).json("No image found");
-      } else {
-        // upload image to bucket
-        const imageResponse = await bucket.upload(postImage.path, {
-          destination: `posts/${postImage.name}`,
-          resumable: true,
-          metadata: {
-            metadata: {
-              firebaseStorageDownloadTokens: uuid,
-            },
-          },
-        });
-
-        // get the image url
-        imageUrl =
-          downLoadPath +
-          encodeURIComponent(imageResponse[0].name) +
-          "?alt=media&token=" +
-          "1";
+      let imageUrl;
+      try {
+        imageUrl = await uploadImage(files.postImage,"posts");
+      } catch (error) {
+        return res.status(400).json(error.message);
       }
-      // console.log("Firebase: Image uploaded.")
 
       // console.log(files, fields)
       // object to send to database
-      if (!fields.userId || !fields.caption || !fields.lattitude || !fields.longitude) {
+      if (
+        !fields.userId ||
+        !fields.caption ||
+        !fields.lattitude ||
+        !fields.longitude
+      ) {
         return res.status(404).json("Empty fields");
-      }
-      else {
+      } else {
         try {
+          console.log(imageUrl)
           const result = await queries.createPost(
             fields.userId,
             fields.caption,
             fields.lattitude,
             fields.longitude,
-            fields.flag ? fields.flag : 0,
-            );
+            fields.flag ? fields.flag : 0
+          );
           //   console.log("SQL: Post created.")
-            await queries.uploadImage(result.insertId, imageUrl);
+          await queries.uploadImage(result.insertId, imageUrl);
           //   console.log("SQL: Image uploaded.")
-            if (fields.tag) {
-              let tag = fields.tag.slice(1, fields.tag.length -1).split(",");
-              if (tag.length > 0) {
-                for (let i = 0; i < tag.length; i++) {
-                  await tagQueries.tagUser(result.insertId, tag[i]);
-                  // console.log("SQL: User " + tag[i] + " tagged.")
-                }
+          if (fields.tag) {
+            let tag = fields.tag.slice(1, fields.tag.length - 1).split(",");
+            if (tag.length > 0) {
+              for (let i = 0; i < tag.length; i++) {
+                await tagQueries.tagUser(result.insertId, tag[i]);
+                // console.log("SQL: User " + tag[i] + " tagged.")
               }
             }
-          return res.status(200).json({message:"Post was created successfully.", url:imageUrl});
+          }
+          return res
+            .status(200)
+            .json({ message: "Post was created successfully.", url: imageUrl });
         } catch (error) {
-          return res.status(400).json({error : error.message});
+          return res.status(400).json({ error: error.message });
         }
       }
     });
@@ -124,68 +90,30 @@ const createPost = async (req, res) => {
   }
 };
 
-
 const updatePFP = async (req, res) => {
   const form = new formidable.IncomingForm({ multiples: true });
   try {
     form.parse(req, async (error, fields, files) => {
-      // create a unique id for the image
-      let uuid = UUID();
-
-      // url of the uploaded image
-      var downLoadPath =
-        "https://firebasestorage.googleapis.com/v0/b/memoriesar-f08a7.appspot.com/o/";
-      
-      
-        const profileImage = files.PFP;
-      if (!profileImage) {
-        return res.status(404).json("No image found");
-      }
-      // rename the image
-      profileImage.name = uuid + profileImage.name;
-
-      // url of the uploaded image
-      let imageUrl;
-
       // check for errors
       if (error) {
         return res.status(400).json(error);
       }
-
-      // get the bucket
-      const bucket = storage.bucket("gs://memoriesar-f08a7.appspot.com");
-
-      // check if image is uploaded
-      if (profileImage.size == 0) {
-        return res.status(404).json("No image found");
-      } else {
-        // upload image to bucket
-        const imageResponse = await bucket.upload(profileImage.path, {
-          destination: `users/${profileImage.name}`,
-          resumable: true,
-          metadata: {
-            metadata: {
-              firebaseStorageDownloadTokens: uuid,
-            },
-          },
-        });
-
-        // get the image url
-        imageUrl =
-          downLoadPath +
-          encodeURIComponent(imageResponse[0].name) +
-          "?alt=media&token=" +
-          "1";
+      let imageUrl;
+      try {
+        imageUrl = await uploadImage(files.postImage,"posts");
+      } catch (error) {
+        return res.status(400).json(error.message);
       }
       // console.log(files, fields)
       // object to send to database
       if (!fields.userId) {
         return res.status(404).json("Empty fields");
-      }
-      else {
+      } else {
         try {
-          await profileQueries.updatePFP(imageUrl, fields.userId);          
-          return res.status(200).json({message:"PFP was updated successfully.", url:imageUrl});
+          await profileQueries.updatePFP(imageUrl, fields.userId);
+          return res
+            .status(200)
+            .json({ message: "PFP was updated successfully.", url: imageUrl });
         } catch (error) {
           return res.status(400).json(error);
         }
@@ -195,6 +123,56 @@ const updatePFP = async (req, res) => {
     return res.status(400).json(error);
   }
 };
+
+const uploadImage = async (profileImage, folderName) => {
+  try {
+    // create a unique id for the image
+    let uuid = UUID();
+
+    // url of the uploaded image
+    var downLoadPath =
+      "https://firebasestorage.googleapis.com/v0/b/memoriesar-f08a7.appspot.com/o/";
+
+    if (!profileImage) {
+      throw new Error("No image found");
+    }
+    // rename the image
+    profileImage.name = uuid + profileImage.name;
+
+    // url of the uploaded image
+    let imageUrl;
+
+    // get the bucket
+    const bucket = storage.bucket("gs://memoriesar-f08a7.appspot.com");
+
+    // check if image is uploaded
+    if (profileImage.size == 0) {
+      throw new Error("No image found");
+    } else {
+      // upload image to bucket
+      const imageResponse = await bucket.upload(profileImage.path, {
+        destination: `${folderName}/${profileImage.name}`,
+        resumable: true,
+        metadata: {
+          metadata: {
+            firebaseStorageDownloadTokens: uuid,
+          },
+        },
+      });
+
+      // get the image url
+      imageUrl =
+        downLoadPath +
+        encodeURIComponent(imageResponse[0].name) +
+        "?alt=media&token=" +
+        "1";
+    }
+    return imageUrl;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
 // router.get("/getUsers", async (req, res, next) => {
 //   await userRef.get().then((value) => {
 //     const data = value.docs.map((doc) => doc.data());
@@ -217,4 +195,4 @@ const updatePFP = async (req, res) => {
 //       });
 //     });
 // });
-module.exports = { createPost , updatePFP};
+module.exports = { createPost, updatePFP, uploadImage };
