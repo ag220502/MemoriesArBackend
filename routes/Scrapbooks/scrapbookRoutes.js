d9857fb7b367cbb09afbe9a145d6dbeee1022780
Davey <91110994+dhd2000@users.noreply.github.com>
2023-03-21 17:34:34 +0400

    scrap

--- routes/Scrapbooks/scrapbookRoutes.js
+++ routes/Scrapbooks/scrapbookRoutes.js
@@ -1,37 +1,45 @@
-const router = require('express').Router();
-const template = require('../../controllers/Scrapbooks/TemplateController.js');
-const category = require('../../controllers/Scrapbooks/categoryController.js');
-const scrapbook = require('../../controllers/Scrapbooks/ScrapbooksController.js');
-const image = require('../../controllers/Scrapbooks/images.js');
+const router = require("express").Router();
+const template = require("../../controllers/Scrapbooks/TemplateController.js");
+const category = require("../../controllers/Scrapbooks/categoryController.js");
+const scrapbook = require("../../controllers/Scrapbooks/ScrapbooksController.js");
+const image = require("../../controllers/Scrapbooks/images.js");
+const { homePage } = require("../../controllers/Scrapbooks/homePage.js");
 
 //template routes
-router.post('/createTemplate', template.createTemplate);
-router.get('/getTemplateById/:templateId', template.getTemplateById);
-router.get('/getAllTemplates', template.getAllTemplates);
-router.patch('/updateTemplate/:templateId', template.updateTemplate);
-router.delete('/deleteTemplate/:templateId', template.deleteTemplate);
+router.post("/createTemplate", template.createTemplate);
+router.get("/getTemplateById/:templateId", template.getTemplateById);
+router.get("/getAllTemplates", template.getAllTemplates);
+router.patch("/updateTemplate/:templateId", template.updateTemplate);
+router.delete("/deleteTemplate/:templateId", template.deleteTemplate);
 
 //category routes
-router.post('/createCategory', category.createCategory);
-router.get('/getCategory/:categoryId', category.getCategory);
-router.get('/getAllCategories', category.getAllCategories);
-router.patch('/updateCategory/:categoryId', category.updateCategory);
-router.delete('/deleteCategory/:categoryId', category.deleteCategory);
+router.post("/createCategory", category.createCategory);
+router.get("/getCategory/:categoryId", category.getCategory);
+router.get("/getAllCategories", category.getAllCategories);
+router.patch("/updateCategory/:categoryId", category.updateCategory);
+router.delete("/deleteCategory/:categoryId", category.deleteCategory);
 
 //scrapbook routes
-router.post('/createScrapbook/:userId', scrapbook.createScrapbook);
-router.patch('/updateScrapbook/:scrapId', scrapbook.updateScrapbook);
-router.get('/getScrapbookById/:scrapId', scrapbook.getScrapbook);
-router.get('/getAllScrapbooks/:userId', scrapbook.getAllUserScrapbooks);
-router.delete('/deleteScrapbook/:scrapId', scrapbook.deleteScrapbook);
-router.delete('/deleteAllScrapbooks/:userId', scrapbook.deleteAllUserScrapbooks);
+router.post("/createScrapbook/:userId", scrapbook.createScrapbook);
+router.patch("/updateScrapbook/:scrapId", scrapbook.updateScrapbook);
+router.get("/getScrapbookById/:scrapId", scrapbook.getScrapbook);
+router.get("/getAllScrapbooks/:userId", scrapbook.getAllUserScrapbooks);
+router.delete("/deleteScrapbook/:scrapId", scrapbook.deleteScrapbook);
+router.delete(
+  "/deleteAllScrapbooks/:userId",
+  scrapbook.deleteAllUserScrapbooks
+);
 
 //image routes
-router.post('/addImage', image.addImage);
-router.get('/getImage/:pictureId', image.getImage);
-router.get('/getAllScrapImages/:scrapId', image.getImageByScrapId);
-router.patch('/updateImage/:pictureId', image.updateImage);
-router.delete('/deleteImage/:pictureId', image.deleteImage);
-router.delete('/deleteAllScrapImages/:scrapId', image.deleteAllScrapImages);
+router.post("/addImage", image.addImage);
+router.get("/getImage/:pictureId", image.getImage);
+router.get("/getAllScrapImages/:scrapId", image.getImageByScrapId);
+router.patch("/updateImage/:pictureId", image.updateImage);
+router.delete("/deleteImage/:pictureId", image.deleteImage);
+router.delete("/deleteAllScrapImages/:scrapId", image.deleteAllScrapImages);
+
+//home page
+
+router.post("/userFeed/:id", homePage);
 
 module.exports = router;
