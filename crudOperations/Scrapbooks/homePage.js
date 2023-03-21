92abc3f45aeb1b0bdf3d4adef29a312aab0e17f3
ag220502 <71845758+ag220502@users.noreply.github.com>
2023-03-21 17:51:47 +0400

    changes

--- crudOperations/Scrapbooks/homePage.js
+++ crudOperations/Scrapbooks/homePage.js
@@ -1,3 +1,16 @@
+/*SELECT ab.userId,ab.profilePhoto,ab.firstName,ab.lastName,ab.id AS postId, ab.coverPhoto,ab.name,ab.ScrapLikes,Count(sc.userId) as ScrapComments from scrapbook_comments sc RIGHT JOIN 
+(
+  SELECT abc.id,Count(sl.userId) as ScrapLikes,abc.userId,abc.profilePhoto,abc.firstName,abc.lastName, abc.coverPhoto,abc.name from scrapbook_likes sl RIGHT JOIN 
+  ( 
+      SELECT DISTINCT(u.id) AS userId, s.scrapId as id, u.profilePhoto,u.firstName,u.lastName, s.coverPhoto,s.name from users u INNER JOIN scrapbooks s INNER JOIN 
+      (
+          SELECT userId,friendId,friendDate FROM `user_friends` WHERE `userId` = 18 OR `friendId` = 18
+      ) v ON 
+      (u.id=v.userId OR u.id=v.friendId) AND u.id=s.userId
+  ) abc ON sl.scrapId=abc.id GROUP BY abc.id
+) ab ON sc.scrapId=ab.id GROUP BY ab.id
+*/
+
 const pool = require("../../connect.js");
 
 const db = {};
@@ -5,7 +18,7 @@ const db = {};
 
 db.getAllScrapbooks = (id) => {
   return new Promise((resolve, reject) => {
-    const query = 
+    const query = "SELECT ab.userId,ab.profilePhoto,ab.firstName,ab.lastName,ab.id AS scrapId, ab.coverPhoto,ab.name,ab.ScrapLikes,Count(sc.userId) as ScrapComments from scrapbook_comments sc RIGHT JOIN ( SELECT abc.id,Count(sl.userId) as ScrapLikes,abc.userId,abc.profilePhoto,abc.firstName,abc.lastName, abc.coverPhoto,abc.name from scrapbook_likes sl RIGHT JOIN ( SELECT DISTINCT(u.id) AS userId, s.scrapId as id, u.profilePhoto,u.firstName,u.lastName, s.coverPhoto,s.name from users u INNER JOIN scrapbooks s INNER JOIN ( SELECT userId,friendId,friendDate FROM `user_friends` WHERE `userId` = ? OR `friendId` = ? ) v ON (u.id=v.userId OR u.id=v.friendId) AND u.id=s.userId ) abc ON sl.scrapId=abc.id GROUP BY abc.id ) ab ON sc.scrapId=ab.id GROUP BY ab.id ORDER BY scrapId DESC"
     pool.query(query, [id, id], (err, results) => {
       if (err) {
         reject(err);
